48b0eeb42c2fbfe3985f562d7f9f0394ff57c3ff
Maxim Arestov <aresmax1993@gmail.com>
2022-12-11 11:25:21 +0300

    Created GetNumber method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+Console.Clear();
+Console.WriteLine("Введите число, означающее количество элементов массива строк:");
+int num = GetNumber();
+string[] arrayStrings = new string[num];
+
+
+
+
+
+int GetNumber()
+{
+    int number = new int();
+    bool check = true;
+    while (check)
+    {
+        bool isParsed = int.TryParse(Console.ReadLine(), out number);
+        if (!isParsed)
+        {
+            Console.WriteLine("Введены некорректные данные. Введите целое число:");
+        }
+        else check = false;
+    }
+    return number;
+}
